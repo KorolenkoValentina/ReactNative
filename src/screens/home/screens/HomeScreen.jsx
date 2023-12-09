b1c078b7f594de95df20c997a4f67b46becc2991
Valentina <108071956+KorolenkoValentina@users.noreply.github.com>
2023-12-09 10:57:47 +0200

    Update HomeScreen.jsx

--- src/screens/home/screens/HomeScreen.jsx
+++ src/screens/home/screens/HomeScreen.jsx
@@ -79,8 +79,8 @@ export default function App(){
 
   const onEndReached = () => {
     if (!isEndReached) {
-    setDataWithRefreshItem((prevData) => [...prevData, ...mockOnEndReachedData.slice(0, 5)]);
-    setIsEndReached(true);
+    setDataWithRefreshItem((prevData) => [...prevData, ...mockOnEndReachedData]);
+    setIsEndReached(true)};
   }
 
 
@@ -91,7 +91,7 @@ export default function App(){
     setFilteredData(filteredItems);
   };
 
-}
+
 
   return (
     <SafeAreaView style={styles.container}>
